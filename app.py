b1a744959bd484e0bd1ef59fb29c94f9f26b91e3
KoteTheInnkeeper <lucianocorrea58@gmail.com>
2021-07-26 15:09:36 -0300

    Updating the logger setup. I learned a bit about this since.

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import logging
+
+# Setting the logger
+# Set the basic configurations for the logger
+logging.basicConfig(format="%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s", level=logging.DEBUG,
+                    filename='log.log')
+# Create the logger
+logger = logging.getLogger("alchemy_project_qt.app")
+
+
 import requests
 
-from data.database_management import Database, logger
+from data.database_management import Database
 
 
 # Get the page content for the effects and ingredients
--- data/database_management.py
+++ data/database_management.py
@@ -9,14 +9,7 @@ from data.database_connection import DatabaseConnection as DatabaseConnect
 from data.database_connection import sqlite3
 from page.ingredients_and_effects import EffectsPage, IngredientsPage
 
-
-
-# Set the basic configurations for the logger
-logging.basicConfig(format="%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s", level=logging.DEBUG,
-                    filename='log.txt')
-# Create the logger
-logger = logging.getLogger("/alchemy_project")
-
+log = logging.getlog("alchemy_project_qt.database_management")
 
 class Database:
     def __init__(self, host: str, alchemy_effects: bytes, alchemy_ingredients: bytes):
@@ -25,41 +18,41 @@ class Database:
             one if it doesn't) to populate the tables if they were empty.
         :return: None
         """
-        logger.debug("Initializing database.")
+        log.debug("Initializing database.")
         self.host = host
-        logger.debug("Checking if the .db file exists.")
+        log.debug("Checking if the .db file exists.")
         # Check if the file exists at all.
         try:
             with open(self.host, 'r'):
-                logger.debug("File exists. Checking for the table's content.")
+                log.debug("File exists. Checking for the table's content.")
         except FileNotFoundError:
             with open(self.host, 'w'):
-                logger.debug("File didn't exist. A new one was created.")
+                log.debug("File didn't exist. A new one was created.")
 
         # Checking for the table's content.
         try:
             with DatabaseConnect(self.host) as cursor:
-                logger.debug("Creating tables if they don't exist.")
+                log.debug("Creating tables if they don't exist.")
                 cursor.execute("CREATE TABLE IF NOT EXISTS effects(effect_name TEXT UNIQUE)")
                 cursor.execute("CREATE TABLE IF NOT EXISTS ingredients(ingredient_name NAME UNIQUE, ef1 INTEGER, "
                                "ef2 INTEGER, ef3 INTEGER, ef4 INTEGER)")
 
-                logger.debug("Seeing if they're empty.")
+                log.debug("Seeing if they're empty.")
                 cursor.execute("SELECT COUNT(*) FROM effects")
                 if cursor.fetchall()[0][0] == 0:
                     input("It appears that the info wasn't downloaded. Press enter to download the potions info.\n")
-                    logger.debug("'effects' table is empty. Populating it...")
+                    log.debug("'effects' table is empty. Populating it...")
                     self.populate_effects(EffectsPage(alchemy_effects))
-                    logger.debug("Since the effects weren't in store, the potions may not be as well. Populating it...")
+                    log.debug("Since the effects weren't in store, the potions may not be as well. Populating it...")
                     self.populate_ingredients(IngredientsPage(alchemy_ingredients))
                 cursor.execute("SELECT COUNT(*) FROM ingredients")
                 if cursor.fetchall()[0][0] == 0:
                     input("The 'effects' are laoded, but the ingredients aren't. Press start to load them.\n")
                     self.populate_ingredients(IngredientsPage(alchemy_ingredients))
         except sqlite3.OperationalError:
-            logger.critical("A sqlite3.OperationalError was raised.")
+            log.critical("A sqlite3.OperationalError was raised.")
             raise
-        logger.debug("Database successfully initialized.")
+        log.debug("Database successfully initialized.")
         print("Database successfully initialized!")
 
     def populate_effects(self, effects_list: EffectsPage) -> None:
@@ -69,13 +62,13 @@ class Database:
         :return: None
         """
         try:
-            logger.debug("Writing the list of effects to the 'effects' table in the database.")
+            log.debug("Writing the list of effects to the 'effects' table in the database.")
             with DatabaseConnect(self.host) as cursor:
                 for effect in effects_list.effects:
                     cursor.execute("INSERT INTO effects VALUES(?)", (effect.ef_name,))
-            logger.debug("Database successfully populated with effects.")
+            log.debug("Database successfully populated with effects.")
         except sqlite3.OperationalError:
-            logger.critical("A sqlite3.OperationalError was raised.")
+            log.critical("A sqlite3.OperationalError was raised.")
 
     def populate_ingredients(self, ingredients_list: IngredientsPage) -> None:
         """
@@ -86,19 +79,19 @@ class Database:
         :param ingredients_list:
         :return: None
         """
-        logger.debug("Writing ingredients to 'ingredients' table in the database.")
+        log.debug("Writing ingredients to 'ingredients' table in the database.")
         with DatabaseConnect(self.host) as cursor:
             for ingredient in ingredients_list.ingredients:
                 name = ingredient.name
                 eff_no = []
-                logger.debug("Getting the 'id' for the effect from the 'effects' table.")
+                log.debug("Getting the 'id' for the effect from the 'effects' table.")
                 for i in range(0, 4):
                     cursor.execute("SELECT ROWID FROM effects WHERE effect_name=?", (ingredient.effects[i],))
                     result = int(cursor.fetchone()[0])
                     eff_no.append(result)
-                logger.debug("Saving the 'id' for each effect, instead of the entire name.")
+                log.debug("Saving the 'id' for each effect, instead of the entire name.")
                 cursor.execute("INSERT INTO ingredients VALUES(?, ?, ?, ?, ?)", (name, eff_no[0], eff_no[1], eff_no[2], eff_no[3]))
-            logger.debug("Database successfully populated with ingredients.")
+            log.debug("Database successfully populated with ingredients.")
 
     def show_ingredients(self):
         """
@@ -109,25 +102,25 @@ class Database:
 
     def show_effects(self, name: str):
         with DatabaseConnect(self.host) as cursor:
-            logger.debug("Searching for matches in ingredient's name.")
+            log.debug("Searching for matches in ingredient's name.")
             cursor.execute("SELECT ef1, ef2, ef3, ef4 FROM ingredients WHERE ingredient_name=?", (name, ))
             results = cursor.fetchone()
             if results:
                 print(f"Possible effects from {string.capwords(name)} are shown below:")
                 for i, effect_no in enumerate(results, start=1):
-                    logger.debug(f"Searching the effect's name indexed as ¨effect_no={effect_no}¨.")
+                    log.debug(f"Searching the effect's name indexed as ¨effect_no={effect_no}¨.")
                     cursor.execute("SELECT effect_name FROM effects WHERE ROWID=?", (int(effect_no),))
                     result = cursor.fetchone()
                     if not any(result):
-                        logger.critical("There was no match? Seek into the table.")
+                        log.critical("There was no match? Seek into the table.")
                     print(f"{i}) {result[0].title()}")
             else:
-                logger.debug(f"We didn't find any match for the given name ¨{name}¨.")
+                log.debug(f"We didn't find any match for the given name ¨{name}¨.")
                 print(f"There was no match for the name you provided ({name.title()}). Check your typing.")
 
     def get_effects_set(self, name: str) -> set:
         with DatabaseConnect(self.host) as cursor:
-            logger.debug(f"Searching if ¨{name}¨ has a match in 'ingredient_name' within the 'ingredients' table.")
+            log.debug(f"Searching if ¨{name}¨ has a match in 'ingredient_name' within the 'ingredients' table.")
             cursor.execute("SELECT ef1, ef2, ef3, ef4 FROM ingredients WHERE ingredient_name=?", (name, ))
             results = cursor.fetchone()
             if results:
@@ -137,7 +130,7 @@ class Database:
 
     def list_effects(self):
         with DatabaseConnect(self.host) as cursor:
-            logger.debug("Showing a list of effects.")
+            log.debug("Showing a list of effects.")
             cursor.execute("SELECT effect_name FROM effects")
             result = cursor.fetchall()
             for i, (effect,) in enumerate(result, start=1):
@@ -145,7 +138,7 @@ class Database:
 
     def get_ingredients_for_effect(self, effect_no):
         if not isinstance(effect_no, int):
-            logger.debug("The effect was given as a string. Finding it's ID...")
+            log.debug("The effect was given as a string. Finding it's ID...")
             with DatabaseConnect(self.host) as cursor:
                 cursor.execute("SELECT ROWID FROM effects WHERE effect_name=?", (effect_no, ))
                 result = cursor.fetchone()
@@ -155,7 +148,7 @@ class Database:
                 else:
                     effect_name = effect_no
                     effect_no = int(result[0])
-        logger.debug(f"The effect's ID is {effect_no}. We shall now check for ingredients with this effect.")
+        log.debug(f"The effect's ID is {effect_no}. We shall now check for ingredients with this effect.")
         with DatabaseConnect(self.host) as cursor:
             cursor.execute("SELECT ingredient_name FROM ingredients WHERE ef1=? OR ef2=? OR ef3=? OR ef4=?", (effect_no, effect_no, effect_no, effect_no))
             result = cursor.fetchall()
